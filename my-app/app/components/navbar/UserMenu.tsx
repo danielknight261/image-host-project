537cc49a6f496a4ce4fc25a8ac1154e590521e43
Dan Knight <danielknight261@gmail.com>
2023-06-02 13:16:14 +0100

    modals login and reg

--- my-app/app/components/navbar/UserMenu.tsx
+++ my-app/app/components/navbar/UserMenu.tsx
@@ -3,8 +3,11 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal()
    const [isOpen, setIsOpen] =useState(false);
 
    const toggleOpen = useCallback(() => {
@@ -15,7 +18,7 @@ const UserMenu = () => {
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div 
-        //   onClick={}
+         onClick={() => {}}
           className="
             hidden
             md:block
@@ -71,6 +74,16 @@ const UserMenu = () => {
           "
         >
           <div className="flex flex-col cursor-pointer">
+            <>
+            <MenuItem 
+            onClick={() => {}}
+            label="Login"
+            />
+             <MenuItem 
+            onClick={registerModal.onOpen}
+            label="Sign up"
+            />
+            </>
            
           </div>
         </div>
